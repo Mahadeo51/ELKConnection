c30b6eeacfb1a05b111769016e1cfbda870e2ab2
MAHADEO RAJAK <38103595+Mahadeo51@users.noreply.github.com>
2021-11-29 14:23:37 +0530

    Update JavaELK.java

--- src/main/java/JavaELK.java
+++ src/main/java/JavaELK.java
@@ -14,44 +14,116 @@ import java.util.Arrays;
 import java.util.Map;
 
 public class JavaELK {
-    public static void main(String[] args){
-        
-        File csvFile =new File("output.csv");
-        //PrintWriter out = new PrintWriter(csvFile);
-        
+     public static void main(String[] args) throws FileNotFoundException{
+        //String filepath="output.txt";
+        //File csvFile =new File("output.csv");
+        File buildcsvFile =new File("build_output.csv");
+        File sonarcsvFile =new File("sonar_output.csv");
+        File cdcsvFile =new File("cd_output.csv");
+
+
+
+        //for connector
         RestHighLevelClient client = new RestHighLevelClient(
                 RestClient.builder(new HttpHost("localhost", 9200, "http")));
 
-        SearchRequest searchRequest = new SearchRequest();
-        searchRequest.indices("emp");
-        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-        searchSourceBuilder.query(QueryBuilders.matchAllQuery());
-        searchRequest.source(searchSourceBuilder);
-        Map<String, Object> map=null;
+        SearchRequest buildsearchRequest = new SearchRequest();
+        buildsearchRequest.indices("build_data");
+        SearchSourceBuilder buildsearchSourceBuilder = new SearchSourceBuilder();
+        buildsearchSourceBuilder.query(QueryBuilders.matchAllQuery());
+        buildsearchRequest.source(buildsearchSourceBuilder);
+        Map<String, Object> buildmap=null;
 
         try {
-            SearchResponse searchResponse = null;
-            searchResponse =client.search(searchRequest, RequestOptions.DEFAULT);
-            if (searchResponse.getHits().getTotalHits().value > 0) {
-                SearchHit[] searchHit = searchResponse.getHits().getHits();
+            SearchResponse buildsearchResponse = null;
+            buildsearchResponse =client.search(buildsearchRequest, RequestOptions.DEFAULT);
+            if (buildsearchResponse.getHits().getTotalHits().value > 0) {
+                SearchHit[] searchHit = buildsearchResponse.getHits().getHits();
                 for (SearchHit hit : searchHit) {
-                    map = hit.getSourceAsMap();
-                    System.out.println("map:"+Arrays.toString(map.entrySet().toArray()));
-                    
-                    //to save data in csv
-                    try(PrintWriter out = new PrintWriter(csvFile)) {
-                        out.println(Arrays.toString(map.entrySet().toArray()));
+                    buildmap = hit.getSourceAsMap();
+                    System.out.println("buildmap:"+ Arrays.toString(buildmap.entrySet().toArray()));
+
+                    try(PrintWriter out = new PrintWriter(buildcsvFile)) {
+                        out.println(Arrays.toString(buildmap.entrySet().toArray()));
                     }
                     catch(Exception e) {
                         System.out.println("Something went wrong." + e.getMessage());
                     }
-
                 }
+
             }
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        //System.exit(0);
+
+        // +++++++++++++++++++++++++++++++++++++ SOnar ++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        SearchRequest sonarsearchRequest = new SearchRequest();
+        sonarsearchRequest.indices("sonar_data");
+        SearchSourceBuilder sonarsearchSourceBuilder = new SearchSourceBuilder();
+        sonarsearchSourceBuilder.query(QueryBuilders.matchAllQuery());
+        sonarsearchRequest.source(sonarsearchSourceBuilder);
+        Map<String, Object> sonarmap=null;
+
+        try {
+            SearchResponse sonarsearchResponse = null;
+            sonarsearchResponse =client.search(sonarsearchRequest, RequestOptions.DEFAULT);
+            if (sonarsearchResponse.getHits().getTotalHits().value > 0) {
+                SearchHit[] searchHit = sonarsearchResponse.getHits().getHits();
+                for (SearchHit hit : searchHit) {
+                    sonarmap = hit.getSourceAsMap();
+                    System.out.println("Sonarmap:"+ Arrays.toString(sonarmap.entrySet().toArray()));
+
+                    try(PrintWriter out = new PrintWriter(sonarcsvFile)) {
+                        out.println(Arrays.toString(sonarmap.entrySet().toArray()));
+                    }
+                    catch(Exception e) {
+                        System.out.println("Something went wrong." + e.getMessage());
+                    }
+                }
+
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        //System.exit(0);
+
+        // +++++++++++++++++++++++++++++++++++ CD ++++++++++++++++++++++++++++++++++++++++++++++++++
+        SearchRequest cdsearchRequest = new SearchRequest();
+        cdsearchRequest.indices("cd_data");
+        SearchSourceBuilder cdsearchSourceBuilder = new SearchSourceBuilder();
+        cdsearchSourceBuilder.query(QueryBuilders.matchAllQuery());
+        cdsearchRequest.source(cdsearchSourceBuilder);
+        Map<String, Object> cdmap=null;
+
+        try {
+            SearchResponse cdsearchResponse = null;
+            cdsearchResponse =client.search(cdsearchRequest, RequestOptions.DEFAULT);
+            if (cdsearchResponse.getHits().getTotalHits().value > 0) {
+                SearchHit[] searchHit = cdsearchResponse.getHits().getHits();
+                for (SearchHit hit : searchHit) {
+                    cdmap = hit.getSourceAsMap();
+                    System.out.println("cdmap:"+ Arrays.toString(cdmap.entrySet().toArray()));
+
+                    try(PrintWriter out = new PrintWriter(cdcsvFile)) {
+                        out.println(Arrays.toString(cdmap.entrySet().toArray()));
+                    }
+                    catch(Exception e) {
+                        System.out.println("Something went wrong." + e.getMessage());
+                    }
+                }
+
+            }
+        }
+        catch (IOException e) {
             e.printStackTrace();
         }
         System.exit(0);
+
     }
+
 
 }
